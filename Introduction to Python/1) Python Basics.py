f69f98740b613d7955e8cc812a1c0cfc4c2003db
MathewJohansson <mathew.johansson1@outlook.com>
2025-02-26 15:20:34 +0000

    modified:   Introduction to Python/1) Python Basics.py

--- Introduction to Python/1) Python Basics.py
+++ Introduction to Python/1) Python Basics.py
@@ -1,1 +1,20 @@
-# 
+# Calculation
+print(4 + 5)
+print(10 / 2)
+print(5 - 5)
+print(3 * 5)
+
+# Variables 
+height = 1.79 
+weight = 68.7 
+bmi = height / weight ** 2
+print(bmi)
+type(bmi)
+# float data type 
+
+# string data type 
+intro = "Hello! How are you?"
+
+# Boolean data type
+is_good = True 
+
