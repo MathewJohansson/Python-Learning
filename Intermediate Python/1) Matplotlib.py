d7e0ff93ac1527c80d5f0a84d9dfd310da89b630
MathewJohansson <mathew.johansson1@outlook.com>
2025-02-28 07:10:50 +0000

    modified:   Intermediate Python/1) Matplotlib.py

--- Intermediate Python/1) Matplotlib.py
+++ Intermediate Python/1) Matplotlib.py
@@ -1,2 +1,13 @@
 # Control structures - necessary to customise the flow of scripts and algorithms. 
 
+# Import, set arrays, basic x-y plot 
+import matplotlib.pyplot as plt
+year = [1950, 1970, 1990, 2010]
+pop = [2.519, 3.692 5.263, 6.972]
+plt.plot(year, pop)
+plt.show()
+
+# Scatterplot
+plt.scatter(year, pop)
+plt.show()
+
